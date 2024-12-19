ad6dd3cd08a7bec6e4b0337cd8aaa6b898f746bf
Mano ANDRIASATARINTSOA <159690397+ManoAndriasat@users.noreply.github.com>
2024-12-19 20:48:47 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "ANDRIASATARINTSOA Manohisoa Alain - Portfolio",
+  title: "Mano Andriasat",
   description: "Portfolio de ANDRIASATARINTSOA Manohisoa Alain alias Mano Andriasat. Découvrez mes projets et réalisations.",
 };
 
